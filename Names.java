00a48b84961669f132c3306f227403cc8f23b7c4
DanielHuynh611 <dungduong.huynh@students.mq.edu.au>
2025-08-06 16:03:33 +1000

    Create Names.java

---
+++ Names.java
@@ -0,0 +1,13 @@
+public class Names {
+	public static void main(String[] Args) {
+		System.out.println("This group consists of:");
+		// Put your signature function on the below line!
+		PrintHarrisSig();
+		// Put your signature function on the above line!
+	}
+	
+	public static void PrintHarrisSig() {
+		System.out.println(".--.     /\\                ____");
+		System.out.println("'--'    /__\\    (^._.^)~ <(o.o )>");
+	}
+}
